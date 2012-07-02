10c05ac2575806d8b1ac201e17b8dadce3280506
Warren Smith <wsmith@tacc.utexas.edu>
2012-07-02 18:54:29 -0500

    completed AMQP step (including SSL support) and adjusted workflows to use it correctly

--- lib/steps/ipf/amqp.py
+++ lib/steps/ipf/amqp.py
@@ -18,9 +18,12 @@
 
 import os
 import random
+import ssl
 import sys
 import time
 
+from ipf.error import NoMoreInputsError
+from ipf.home import IPF_HOME
 from ipf.step import Step
 
 from mtk.amqp_0_9_1 import *
@@ -35,12 +38,20 @@ class AmqpPublishStep(Step):
         self.name = "ipf/publish/amqp"
         self.description = "publishes documents by via AMQP"
         self.time_out = 5
-        self.accepts_params["services"] = "A list of services to try to connect to. Each is a dictionary with keys: host, port (optional), ssl_options (optional). The step will try to publish each document to one of these services."
+        self.accepts_params["services"] = "A list of services to try to connect to. Each item is host[:port]. If no port is specified, port 5672 will be used for TCP connections and port 5671 will be used for SSL connections."
+        self.accepts_params["username"] = "the user to authenticate as"
+        self.accepts_params["password"] = "the password to authenticate with"
+        self.accepts_params["ssl_options"] = "A dictionary containing the SSL options to use to connect. See the Python ssl.wrap_socket function for keys and values. Any relative path names are relative to $IPF_HOME/etc"
         self.accepts_params["vhost"] = "the AMQP virtual host to connect to"
         self.accepts_params["exchange"] = "the AMQP exchange to publish to"
-        # credentials for ssl
+
+        if "requires_types" in params:
+            self.requires_types.extend(params["requires_types"])
 
         self.services = []
+        self.username = None
+        self.password = None
+        self.ssl_options = None
         self.vhost = None
         self.exchange = None
 
@@ -56,33 +67,64 @@ class AmqpPublishStep(Step):
         except KeyError:
             raise StepError("services parameter not specified")
         try:
-            self.vhost = self.params["vhost"]
+            self.username = self.params["username"].encode("utf-8")
+        except KeyError:
+            self.username = "guest"
+        try:
+            self.password = self.params["password"].encode("utf-8")
+        except KeyError:
+            self.password = "guest"
+        try:
+            self.ssl_options = self.params["ssl_options"]
+            try:
+                if not os.path.isabs(self.ssl_options["keyfile"]):
+                    self.ssl_options["keyfile"] = os.path.join(IPF_HOME,"etc",self.ssl_options["keyfile"])
+            except KeyError:
+                pass
+            try:
+                if not os.path.isabs(self.ssl_options["certfile"]):
+                    self.ssl_options["certfile"] = os.path.join(IPF_HOME,"etc",self.ssl_options["certfile"])
+            except KeyError:
+                pass
+            try:
+                if not os.path.isabs(self.ssl_options["ca_certs"]):
+                    self.ssl_options["ca_certs"] = os.path.join(IPF_HOME,"etc",self.ssl_options["ca_certs"])
+            except KeyError:
+                pass
+            if "ca_certs" in self.ssl_options and "cert_reqs" not in self.ssl_options:
+                self.ssl_options["cert_reqs"] = ssl.CERT_REQUIRED
+        except KeyError:
+            self.ssl_options = None
+
+        try:
+            self.vhost = self.params["vhost"].encode("utf-8")
         except KeyError:
             self.vhost = "/"
         try:
-            self.exchange = self.params["exchange"]
+            self.exchange = self.params["exchange"].encode("utf-8")
         except KeyError:
             self.exchange = ""
 
-        try:
-            while True:
-                doc = self.input_queue.get(True)
+        more_inputs = True
+        while more_inputs:
+            doc = self.input_queue.get(True)
+            if doc == Step.NO_MORE_INPUTS:
+                more_inputs = False
+            else:
                 self._publish(doc)
-        except NoMoreInputsError:
-            pass  # that's ok, this step is done
 
     def _publish(self, doc):
         self.info("publishing document of type %s",doc.type)
         self._connectIfNecessary()
         if self.channel is None:
-            logger.error("not connected to any service, will not publish document of type %s",doc.type)
+            self.error("not connected to any service, will not publish document of type %s",doc.type)
             return
         try:
-            self.channel.basicPublish(doc.body,self.exchange,doc.id)
+            self.channel.basicPublish(doc.body,self.exchange,doc.id.encode("utf-8"))
         except AmqpError:
-            logger.warning("first publish failed, will try to another service")
+            self.warning("first publish failed, will try to another service")
             self._connect()
-            self.channel.basicPublish(doc.body,self.exchange,doc.id)
+            self.channel.basicPublish(doc.body,self.exchange,doc.id.encode("utf-8"))
 
     def _connectIfNecessary(self):
         if self.channel is not None:
@@ -91,39 +133,47 @@ class AmqpPublishStep(Step):
             try:
                 self._connect()
                 return
-            except AmqpError:
-                logger.warn("failed to connect to %s",self.channel["host"])
+            except MtkError, e:
+                self.warning("failed to connect to service: %s",e)
 
     def _connect(self):
-        if self.channel is not None:
-            try:
-                self.channel.close()
-            except AmqpError:
-                pass
-            self.channel = None
         if self.connection is not None:
             try:
                 self.connection.close()
-            except AmqpError:
+            except:
                 pass
             self.connection = None
+            self.channel = None
 
         service = self._selectService()
+        toks = service.split(":")
+        host = toks[0]
         try:
-            host = service["host"]
-        except KeyError:
-            host = "localhost"
-        try:
-            port = service["port"]
-        except KeyError:
-            if "ssl_options" not in service:
+            port = int(toks[1])
+        except:
+            if self.ssl_options is None:
                 port = 5672
             else:
                 port = 5671
-        if "ssl_options" not in service:
-            self.connection = Connection(host,port,self.vhost)
+                
+        if self.ssl_options is None:
+            self.connection = Connection(host,
+                                         port,
+                                         self.vhost,
+                                         PlainMechanism(self.username,self.password))
         else:
-            self.connection = Connection(host,port,self.vhost,X509Mechanism(),ssl_options)
+            if "keyfile" in self.ssl_options:
+                self.connection = Connection(host,
+                                             port,
+                                             self.vhost,
+                                             X509Mechanism(),
+                                             self.ssl_options)
+            else:
+                self.connection = Connection(host,
+                                             port,
+                                             self.vhost,
+                                             PlainMechanism(self.username,self.password),
+                                             self.ssl_options)
         self.channel = self.connection.channel()
 
     def _selectService(self):
