ad306e812aed20b32830c38bd0a573e8b6aef618
Warren Smith <wsmith@tacc.utexas.edu>
2012-08-17 07:45:29 -0500

    fixed to work with changes from the last big refactoring

--- libexec/step_info.py
+++ libexec/step_info.py
@@ -26,41 +26,32 @@ if sys.version_info < min_version or sys.version_info > max_version:
     print(stderr,"Python version 2.6 or 2.7 is required")
     sys.exit(1)
 
-from ipf.engine import WorkflowEngine
+from ipf.catalog import catalog
 
 #######################################################################################################################
-
-def usage():
-    print("usage: step_info.py [step type]*")
 
 if __name__ == "__main__":
     opt = optparse.OptionParser(usage="usage: %prog [options]")
     opt.add_option("-n","--name_pattern",action="store",type="string",dest="name_pattern",default=".*",
                    help="a Python regular expression to use to select steps by name (default .*)")
-    opt.add_option("-c","--class_pattern",action="store",type="string",dest="class_pattern",default=".*",
-                   help="a Python regular expression to use to select steps by module.class (default .*)")
-    opt.add_option("-i","--input_type_pattern",action="store",type="string",dest="input_type_pattern",default=".*",
+    opt.add_option("-i","--input_pattern",action="store",type="string",dest="input_pattern",default=".*",
                    help="a Python regular expression to use to select steps by type of input document (default .*)")
-    opt.add_option("-o","--output_type_pattern",action="store",type="string",dest="output_type_pattern",default=".*",
+    opt.add_option("-o","--output_pattern",action="store",type="string",dest="output_pattern",default=".*",
                    help="a Python regular expression to use to select steps by type of output document (default .*)")
     (options,args) = opt.parse_args()
 
-    engine = WorkflowEngine()
-    steps = engine.readKnownSteps()
-    for step_name in steps:
-        step = steps[step_name]({})
+    for step_name in sorted(catalog.steps):
+        step = catalog.steps[step_name]()
         if re.search(options.name_pattern,step.name) == None:
             continue
-        if re.search(options.class_pattern,step.__module__+"."+step.__class__.__name__) == None:
-            continue
         if not reduce(lambda b1,b2: b1 or b2,
-                      map(lambda type: re.search(options.input_type_pattern,type) is not None,
-                          step.requires_types),
+                      map(lambda cls: re.search(options.input_pattern,cls.__module__+"."+cls.__name__) is not None,
+                          step.requires),
                       False):
             continue
         if not reduce(lambda b1,b2: b1 or b2,
-                      map(lambda type: re.search(options.output_type_pattern,type) is not None,
-                          step.produces_types),
+                      map(lambda cls: re.search(options.output_pattern,cls.__module__+"."+cls.__name__) is not None,
+                          step.produces),
                       False):
             continue
         print(step)
