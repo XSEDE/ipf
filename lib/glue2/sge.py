ae9038039deb95595c90d4c49ed7460fdb35083f
Warren Smith <wsmith@tacc.utexas.edu>
2013-01-09 10:35:11 -0600

    removed stray prints

--- lib/glue2/sge.py
+++ lib/glue2/sge.py
@@ -375,7 +375,6 @@ class ComputingActivityUpdateStep(glue2.computing_activity.ComputingActivityUpda
         watcher.run()
 
     def _logEntry(self, log_file_name, line):
-        print("new log entry")
         if line.startswith("#"):
             return
 
@@ -496,7 +495,6 @@ class ComputingActivityUpdateStep(glue2.computing_activity.ComputingActivityUpda
         else:
             self.warning("unknown job log of type %s" % toks[3])
 
-        print(activity)
         if self._includeQueue(activity.Queue):
             self.output(activity)
 
