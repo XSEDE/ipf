b60feca0f94c275341c7b5d982366c5c150410ea
Warren Smith <wwsmith@tg-login1.blacklight.psc.teragrid.org>
2013-05-24 09:32:15 -0400

    added support for modules (in addition to lmod)

--- lib/glue2/modules.py
+++ lib/glue2/modules.py
@@ -42,11 +42,10 @@ class LModApplicationsStep(glue2.application.ApplicationsStep):
 
         module_paths = []
         try:
-            #paths = os.environ["LMOD_DEFAULT_MODULEPATH"]
             paths = os.environ["MODULEPATH"]
             module_paths.extend(paths.split(":"))
         except KeyError:
-            raise StepError("didn't find environment variable LMOD_DEFAULT_MODULEPATH")
+            raise StepError("didn't find environment variable MODULEPATH")
 
         apps = glue2.application.Applications(self.resource_name)
         for path in module_paths:
@@ -94,6 +93,100 @@ class LModApplicationsStep(glue2.application.ApplicationsStep):
         handle = glue2.application.ApplicationHandle()
         handle.Type = ApplicationHandle.MODULE
         handle.Value = name+"/"+version
+
+        apps.add(env,[handle])
+
+#######################################################################################################################
+
+class ModulesApplicationsStep(glue2.application.ApplicationsStep):
+    def __init__(self):
+        glue2.application.ApplicationsStep.__init__(self)
+
+        self._acceptParameter("exclude","a comma-separated list of modules to ignore (default is to ignore none)",
+                              False)
+
+    def _run(self):
+        try:
+            self.exclude = self.params["exclude"].split(",")
+        except KeyError:
+            self.exclude = []
+
+        apps = glue2.application.Applications(self.resource_name)
+
+        module_paths = []
+        try:
+            paths = os.environ["MODULEPATH"]
+            module_paths.extend(paths.split(":"))
+        except KeyError:
+            raise StepError("didn't find environment variable MODULEPATH")
+
+        for path in module_paths:
+            self._addPath(path,path,module_paths,apps)
+
+        return apps
+
+    def _addPath(self, path, module_path, module_paths, apps):
+        try:
+            file_names = os.listdir(path)
+        except OSError:
+            return
+        for name in file_names:
+            if os.path.join(path,name) in module_paths:
+                # don't visit other module paths
+                continue
+            if os.path.isdir(os.path.join(path,name)):
+                self._addPath(os.path.join(path,name),module_path,module_paths,apps)
+            else:
+                self._addModule(os.path.join(path,name),module_path,apps)
+    
+    def _addModule(self, path, module_path, apps):
+        if os.path.split(path)[1].startswith("."):
+            return
+        if path.endswith("~"):
+            return
+
+        #print(path)
+
+        file = open(path)
+        lines = file.readlines()
+        file.close()
+
+        if not lines[0].startswith("#%Module"):
+            return
+
+        env = glue2.application.ApplicationEnvironment()
+
+        str = path[len(module_path)+1:]
+        slash_pos = str.find("/")  # assumes Unix-style paths
+        if slash_pos == -1:
+            env.AppName = str
+            env.AppVersion = None
+        else:
+            env.AppName = str[:slash_pos]
+            env.AppVersion = str[slash_pos+1:]
+
+        handle = glue2.application.ApplicationHandle()
+        handle.Type = ApplicationHandle.MODULE
+        if env.AppVersion is None:
+            handle.Value = env.AppName
+        else:
+            handle.Value = env.AppName+"/"+env.AppVersion
+
+        description = ""
+        for line in lines:
+            m = re.search("puts stderr \"([^\"]+)\"",line)
+            if m is not None:
+                if description != "":
+                    description += " "
+                description += m.group(1)
+        if description != "":
+            description = description.replace("$_module_name",handle.Value)
+            if env.AppVersion is not None:
+                description = description.replace("$version",env.AppVersion)
+            description = description.replace("\\t"," ")
+            description = description.replace("\\n","")
+            description = re.sub(" +"," ",description)
+            env.Description = description
 
         apps.add(env,[handle])
 
