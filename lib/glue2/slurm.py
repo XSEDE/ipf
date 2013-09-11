1b146a4823a8c38807b790166c2153b711602747
Warren Smith <wsmith@tacc.utexas.edu>
2013-09-11 10:51:31 -0500

    fixed regex error for Account

--- lib/glue2/slurm.py
+++ lib/glue2/slurm.py
@@ -118,7 +118,7 @@ def _getJob(step, job_str):
     m = re.search("UserId=(\S+)\(",job_str)
     if m is not None:
         job.LocalOwner = m.group(1)
-    m = re.search("Account=(\S+)\(",job_str)
+    m = re.search("Account=(\S+)",job_str)
     if m is not None:
         job.UserDomain = m.group(1)
     m = re.search("Partition=(\S+)",job_str)
