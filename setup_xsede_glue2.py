818ccb28f03407da7f2aabdcd3176e43921d0104
Warren Smith <wsmith@tacc.utexas.edu>
2014-10-19 22:32:09 -0500

    separate setup scripts for xsede

---
+++ setup_xsede_glue2.py
@@ -0,0 +1,49 @@
+
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+def _getManifestFileName():
+    path = os.path.abspath(__file__)
+    path = os.path.split(path)[0]    # drop file name
+    return os.path.join(path,"MANIFEST.in")
+
+def _createManifest():
+    f = open(_getManifestFileName(),"w")
+    f.write("""
+include ipf/etc/ipf/xsede/ca_certs.pem
+include ipf/etc/ipf/workflow/xsede/glue2/*.json
+    """)
+    f.close()
+
+def _deleteManifest():
+    os.remove(_getManifestFileName())
+
+if __name__ == "__main__":
+    _createManifest()
+    description="XSEDE GLUE v2.0 workflows for the Information Publishing Framework"
+    setup(name="ipf-xsede-glue2",
+          #version="1.0"
+          version="1.0a9",
+          description=description,
+          long_description=description,
+          classifiers=[
+              #"Development Status :: 5 - Production/Stable",
+              #"Development Status :: 4 - Beta",
+              "Development Status :: 3 - Alpha",
+              "License :: OSI Approved :: Apache Software License",
+              "Programming Language :: Python :: 2",
+              "Topic :: System :: Monitoring",
+          ],
+          keywords="monitoring information gathering publishing glue2 xsede",
+          url="https://bitbucket.org/wwsmith/ipf",
+          author="Warren Smith",
+          author_email="wsmith@tacc.utexas.edu",
+          license="Apache",
+          packages=["ipf.teragrid"],
+          install_requires=["ipf"],
+          include_package_data=True,
+          zip_safe=False)
+    _deleteManifest()
+
+
--- setup_xsede_kits.py
+++ setup_xsede_kits.py
@@ -3,21 +3,6 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-# install locally as links back to this directory
-# python setup.py develop
-# python setup.py develop --uninstall
-
-# install in editable mode for development/testing pip install -e .
-
-# Create a pypi account and have a $HOME/.pypirc with that info
-
-# To register the project in pypi (just once):
-# $ python setup.py register
-
-# To create a source distribution and upload to pypi:
-# $ python setup.py sdist upload
-
-
 def readme():
     with open("README.rst") as f:
         return f.read()
@@ -30,28 +15,22 @@ def _getManifestFileName():
 def _createManifest():
     f = open(_getManifestFileName(),"w")
     f.write("""
-include README.rst
-include LICENSE-2.0.txt
-include ipf/etc/ipf/logging.conf
-include ipf/etc/ipf/workflow/*.json
-include ipf/etc/ipf/workflow/glue2/*.json
-include ipf/etc/init.d/ipf-WORKFLOW
-include ipf/var/ipf/README.txt
-include ipf/var/log/ipf/README.txt
+include ipf/etc/ipf/workflow/xsede/teragrid_software.json
+include ipf/etc/ipf/workflow/xsede/teragrid_software_periodic.json
     """)
     f.close()
 
 def _deleteManifest():
     os.remove(_getManifestFileName())
 
-
 if __name__ == "__main__":
     _createManifest()
-    setup(name="ipf",
+    description="XSEDE workflows to publish TeraGrid kits and local software using the Information Publishing Framework"
+    setup(name="ipf-xsede-teragrid-kitssoftware",
           #version="1.0"
           version="1.0a9",
-          description="The Information Publishing Framework",
-          long_description=readme(),
+          description=description,
+          long_description=description,
           classifiers=[
               #"Development Status :: 5 - Production/Stable",
               #"Development Status :: 4 - Beta",
@@ -60,21 +39,16 @@ if __name__ == "__main__":
               "Programming Language :: Python :: 2",
               "Topic :: System :: Monitoring",
           ],
-          keywords="monitoring information gathering publishing glue2",
+          keywords="monitoring information gathering publishing glue2 xsede",
           url="https://bitbucket.org/wwsmith/ipf",
           author="Warren Smith",
           author_email="wsmith@tacc.utexas.edu",
           license="Apache",
-          packages=["ipf","ipf.glue2"],
-          install_requires=["mtk"],
-          entry_points={
-              "console_scripts": ["ipf_workflow=ipf.run_workflow:main"],
-          },
+          packages=[],
+          install_requires=["ipf-xsede-glue2"],
           include_package_data=True,
           zip_safe=False)
     _deleteManifest()
-
-
 
 
 
